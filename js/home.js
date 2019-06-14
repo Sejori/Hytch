ec919a4a9e23ae9ce9ebb3f6765464b552697b6e
Seb Ringrose <sebringrose@gmail.com>
2019-06-14 11:45:08 +0100

    relative home links + tracking code

--- js/home.js
+++ js/home.js
@@ -1,7 +1,14 @@
+function setupNavLinks() {
+  document.querySelector("#home-button").href = window.location.origin
+  document.querySelector("#about-button").href = window.location.origin + "/about"
+  return
+}
+
 function goToRegion() {
   let regionSelector = document.querySelector("#region-selector")
   let region = regionSelector.value
   document.location.assign('/' + region)
 }
 
+setupNavLinks()
 Array.from(document.getElementsByClassName("region-submit")).forEach(button => button.addEventListener("click", goToRegion))
